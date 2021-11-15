2d656e21afce9c492abd66a10d04065763016fbb
Mike <wattonlaw@gmail.com>
2021-11-14 23:34:39 -0600

    commit

---
+++ let_const_exercise.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+
+
+// 1. let is block-scoped (so it only exists inside a code block) and can be reassigned but not redeclared. var is neither and will hoist to the top of the scope it's in. 
+
+// 2. let can be reassigned but const can not be reassigned (or redeclared).
+
+// 3. var can be reassigned and redeclared, and const can do neither.
+
+// 4. Hoisting is when something rises to the top of the scope it's in and is processed before anything else. 
+
